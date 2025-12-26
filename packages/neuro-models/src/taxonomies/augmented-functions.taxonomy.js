b7d4a20f3ff6552ce6c8a50076913b3baabd04a6
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-26 07:27:50 -0700

    Create augmented-functions.taxonomy.js

---
+++ packages/neuro-models/src/taxonomies/augmented-functions.taxonomy.js
@@ -0,0 +1,20 @@
+// /packages/neuro-models/src/taxonomies/augmented-functions.taxonomy.js
+export const augmentedFunctionTypes = [
+  "sensory_extension",
+  "memory_support",
+  "motor_assist",
+  "cognitive_scaling",
+  "emotional_regulation",
+  "unknown_experimental",
+];
+
+export const unknownFunctionTemplate = {
+  id: null,
+  label: "unlabeled_function",
+  description: "",
+  riskLevel: "UNKNOWN",
+  inputModalities: [],
+  outputModalities: [],
+  reversible: true,
+  requiresOversight: true,
+};
