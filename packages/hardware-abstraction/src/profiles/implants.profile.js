cd135d0871b50e3e6c40ec9e3a26c38a0b8c0d14
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-26 07:43:13 -0700

    Update implants.profile.js

--- packages/hardware-abstraction/src/profiles/implants.profile.js
+++ packages/hardware-abstraction/src/profiles/implants.profile.js
@@ -1,9 +1,10 @@
 // /packages/hardware-abstraction/src/profiles/implants.profile.js
 export const implantHardwareProfile = {
+  id: "implant_generic_v1",
   class: "implant",
   latencyMs: 5,
   bandwidthKbps: 256,
   channelTypes: ["spike_train", "local_field_potential"],
   closedLoopCapable: true,
-  invasive: true,
+  invasive: true
 };
