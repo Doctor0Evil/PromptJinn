88af1d34389f0d4e574db2cbda5170d2f24eaa96
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-26 07:39:14 -0700

    Create index.js

---
+++ packages/neuro-models/src/index.js
@@ -0,0 +1,4 @@
+// /packages/neuro-models/src/index.js
+export * from "./taxonomies/augmented-functions.taxonomy.js";
+export * from "./models/unknown-function-registry.js";
+export * from "./services/hypothesis-engine.js";
