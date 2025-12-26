aaaae07645678b2159c8e262d1d434eb2ec5b493
Jacob Scott Farmer <xboxteejaymcfarmer@gmail.com>
2025-12-26 07:42:29 -0700

    Create index.js

---
+++ packages/hardware-abstraction/src/index.js
@@ -0,0 +1,4 @@
+// /packages/hardware-abstraction/src/index.js
+export * from "./profiles/implants.profile.js";
+export * from "./profiles/wearables.profile.js";
+export * from "./adapters/generic-bci.adapter.js";
